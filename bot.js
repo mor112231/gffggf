1392f21aa1d6df78069cd14204bf6539bc41c04e
mor112231 <47279690+mor112231@users.noreply.github.com>
2019-02-15 02:34:45 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,12 @@
+-*+-const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream")
+
+client.on("ready", () => {
+let channel =     client.channels.get("484386725787860992")
+setInterval(function() {
+channel.send(`mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_#mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_# mor1122 @_#`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
